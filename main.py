bc12bbc5986e120936532766a93f44f82272755d
Muhammad-Ohee <md.ohee786@gmail.com>
2023-07-26 22:08:11 +0600

    Version 1

---
+++ main.py
@@ -0,0 +1,20 @@
+# 🚨 Don't change the code below 👇
+row1 = ["💙", "💛", "💚"]
+row2 = ["💙", "💛", "💚"]
+row3 = ["💙", "💛", "💚"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+
+row = int(position[0])
+coloum = int(position[1])
+map[coloum-1][row-1] = '🏆'
+
+#Write your code above this row 👆
+
+# 🚨 Don't change the code below 👇
+print(f"{row1}\n{row2}\n{row3}")
+
